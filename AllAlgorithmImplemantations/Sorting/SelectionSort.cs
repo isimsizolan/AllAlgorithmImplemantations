eccee32a0108f7999fb63d3bad9daecd5180e688
Alper Eğitmen <alper.egitmen@paydustry.com>
2022-01-16 21:09:14 +0300

    Add project files.

---
+++ AllAlgorithmImplemantations/Sorting/SelectionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AllAlgorithmImplemantations.Sorting
+{
+    public static class SelectionSortExtention
+    {
+        public static int[] SelectionSort(this int[] arr )
+        {
+
+            int N = arr.Length;
+            int min;
+
+            for (int i = 0; i < N; i++)
+            {
+                min = i;
+                for (int j = i; j < N; j++)
+                {
+                    min = arr[j] < arr[min] ? j : min;
+                }
+
+                int tmp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = tmp;
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ Driver/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AllAlgorithmImplemantations.Sorting;
+
+namespace Driver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 5, 1, 1, 12, 3 };
+
+            arr = arr.SelectionSort();
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
