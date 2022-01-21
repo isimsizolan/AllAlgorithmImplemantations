38c522e17c2624aede24e139fc05d1fd111101d6
aegitmen <aegitmen@yildiz.edu.tr>
2022-01-21 20:45:04 +0300

    bubble sort initial commit

---
+++ AllAlgorithmImplemantations/Sorting/BubbleSort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AllAlgorithmImplemantations.Sorting
+{
+    public static class BubbleSortExtention
+    {
+        public static int[] BubbleSort(this int[] arr)
+        {
+            return arr;
+        }
+    }
+}
