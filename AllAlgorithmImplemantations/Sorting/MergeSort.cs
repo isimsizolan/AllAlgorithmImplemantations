bffcd52b03153f8fdee85874890938deb005951b
aegitmen <aegitmen@yildiz.edu.tr>
2022-01-21 20:40:43 +0300

    MergeSort top-down implementation

--- AllAlgorithmImplemantations/Sorting/MergeSort.cs
+++ AllAlgorithmImplemantations/Sorting/MergeSort.cs
@@ -8,7 +8,40 @@ namespace AllAlgorithmImplemantations.Sorting
     {
         public static int[] MergeSort(this int[] arr)
         {
+            int[] aux = new int[arr.Length];
+            sort(arr, aux, 0, arr.Length-1);
             return arr;
+        }
+
+        public static void sort(int[] arr, int[] aux, int low, int high)
+        {
+            if (high <= low)
+                return;
+            int mid = low + (high - low) / 2;
+            sort(arr, aux, low, mid);
+            sort(arr, aux, mid + 1, high);
+            merge(arr, aux, low, mid, high);
+        }
+
+        public static void merge(int[] arr, int[] aux, int low, int mid, int high)
+        {
+            int i = low;
+            int j = mid + 1;
+
+            for (int k = low; k <= high; k++)
+                aux[k] = arr[k];
+
+            for (int k = low; k <= high; k++)
+            {
+                if (i > mid)
+                    arr[k] = aux[j++];
+                else if (j > high)
+                    arr[k] = aux[i++];
+                else if (aux[i] < aux[j])
+                    arr[k] = aux[i++];
+                else
+                    arr[k] = aux[j++];
+            }
         }
     }
 }
--- Driver/Program.cs
+++ Driver/Program.cs
@@ -10,7 +10,7 @@ namespace Driver
 
             int[] arr = { 5, 1, 1, 12, 3 ,0 };
 
-            arr = arr.InsertionSort();
+            arr = arr.MergeSort();
 
 
             foreach (var item in arr)
