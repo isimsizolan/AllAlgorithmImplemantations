28d5e371c4e1889aac802b70b0b2533baf07165e
aegitmen <aegitmen@yildiz.edu.tr>
2022-02-06 19:47:33 +0300

    heap sort initial commit with bad practice

--- AllAlgorithmImplemantations/Sorting/HeapSort.cs
+++ AllAlgorithmImplemantations/Sorting/HeapSort.cs
@@ -6,10 +6,59 @@ namespace AllAlgorithmImplemantations.Sorting
 {
     public static class HeapSortExtention
     {
-
-        public static void Sort(this int[] arr)
+        public static void HeapSort(this int[] arr)
         {
-            throw new NotImplementedException();
+            //TODO: array start with index 0, but tree root node start with 1. this is quick but bad solution to fix this.
+            int[] temparray = new int [arr.Length+1];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                temparray[i] = arr[i - 1];
+            }
+
+            int N = arr.Length;
+            for (int i = N/2; i >=1; i--)
+            {
+                sink(temparray, i, N);
+            }
+            while(N > 1)
+            {
+                exch(temparray, 1, N--);
+                sink(temparray, 1, N);
+            }
+
+            //TODO:
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = temparray[i + 1];
+            }
+        }
+
+        private static void sink(int[] arr, int i, int n)
+        {
+            while (i * 2 <= n)
+            {
+                int j = i*2;
+                if (j < n && less(arr[j], arr[j + 1]))
+                    j++;
+                if (!less(arr[i], arr[j])) break;
+                exch(arr, i, j);
+                i = j;
+            }
+        }
+
+        private static void exch(int[] arr, int v1, int v2)
+        {
+            int tmp = arr[v1];
+            arr[v1] = arr[v2];
+            arr[v2] = tmp;
+        }
+
+        private static bool less(int j, int v)
+        {
+            if (j < v)
+                return true;
+
+            return false;
         }
     }
 }
--- Driver/Program.cs
+++ Driver/Program.cs
@@ -8,9 +8,8 @@ namespace Driver
         static void Main(string[] args)
         {
 
-            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-            arr.BubbleSort();
+            int[] arr = { 0,5,2,1,4,56,3,12,5,6,78};
+            arr.HeapSort();
 
             foreach (var item in arr)
             {
